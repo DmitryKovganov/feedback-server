846ce0440e25720603425282eb4d65ecdafbbbb0
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-12-06 20:01:39 +0300

    add landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Emaily
+            </h1>
+            <span>Collect feedback from your users</span>
+        </div>
+    );
+};
+
+export default Landing;
