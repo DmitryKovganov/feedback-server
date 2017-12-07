d83b8e90f43afba2883e77e264a35f3e3f733469
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-12-07 19:40:31 +0300

    add credits to user and implement post charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,18 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
-        req.logout();
-        res.redirect('/');
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '5$ for 5 credits',
+            course: req.body.id
+        });
+
+        req.user.cdredits += 5;
+        const user = await req.user.save();
+
+        req.send(user);
     });
 };
