f3bd6d905137e5f8a15db7b368e31082994afcec
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-12-07 20:30:57 +0300

    add heroku build setup

--- index.js
+++ index.js
@@ -24,5 +24,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // прод бандл и прочие файлы брать по этому пути
+    app.use(express.static('client/build'));
+
+    // если не распознает роут, то отдаст индекс
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
