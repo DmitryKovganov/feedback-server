f351e0b50f1c4df8baa511114c9b4161658eefb4
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2017-12-31 12:54:57 +0300

    add render fields for form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,27 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+    { name: 'title', label: 'Survey Title' },
+    { name: 'subject', label: 'Subject Line' },
+    { name: 'body', label: 'Email Body' },
+    { name: 'email', label: 'Recipient List' }
+];
+
 class SurveyForm extends Component {
+    renderFields() {
+        return _.map(FIELDS, ({ name, label }) => {
+            return <Field key={name} type="text" component={SurveyField} name={name} label={label}/>
+        });
+    }
+
     render() {
         return (
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-                    <Field type="text" name="surveyTitle" component={SurveyField} />
+                    {this.renderFields()}
                     <button type="submit">Submit</button>
                 </form>
             </div>
